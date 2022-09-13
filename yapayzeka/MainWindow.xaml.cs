b7812c0b458a0f11d408723d4a9c174ad7e7ab00
Joshghun <92180728+joshgun420@users.noreply.github.com>
2022-09-13 13:22:19 +0400

    Add project files.

---
+++ yapayzeka/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AIMLbot;
+namespace yapayzeka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Bot AI=new Bot();
+            AI.loadSettings();
+            AI.loadAIMLFromFiles();
+            AI.isAcceptingUserInput = false;
+            User myuser=new User("Username Here",AI);
+            AI.isAcceptingUserInput=true;
+            Request r = new Request(textbox2.Text, myuser, AI);
+            Result res=AI.Chat(r);
+            textbox1.Text="Bot: "+res.Output;
+
+        }
+    }
+}
